94548af0267f6a695414268e6fd12e90de2bf754
grynka <ihor.kozhemyakin@gmail.com>
2022-09-01 23:53:33 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (const item of ingredients) {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = item;
+  ingredient.className = 'item';
+  list.append(ingredient);
+}
