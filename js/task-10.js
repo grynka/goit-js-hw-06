d6cb03c9a5bf8ede9af44709d96a477789b45493
grynka <ihor.kozhemyakin@gmail.com>
2022-09-03 02:56:05 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
+const nums = document.querySelector("input");
+const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector("#boxes");
+const amount = nums.value; 
+
+buttonCreate.addEventListener("click", createBoxes);
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = nums.value;
+   for (let i = 0; i < amount; i += 1) {
+    const item = document.createElement("div");
+    item.style.width = 30 + i * 10 + "px";
+    item.style.height = 30 + i * 10 + "px";
+    item.style.backgroundColor = getRandomHexColor();
+   boxes.append(item);
+  }
+
+}
+
+function destroyBoxes() {
+boxes.innerHTML  = "";
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
